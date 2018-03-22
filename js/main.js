17cfbc09d2c48ba2aa2545dcb81e9db09b0fc462
LynetteMar <lynettemartina@gmail.com>
2018-03-22 20:44:46 +0100

    final details done

--- js/main.js
+++ js/main.js
@@ -16,13 +16,26 @@ function checkTime(i) {
 
 var txt = document.getElementById('txt');
 
+//clock animation
+
 var tl1 = new TimelineMax();
 tl1.to(txt, 2, {fontSize: 50});
 //tl1.from(mainHeading, 2, {fontSize: 100});
 
+//sun and moon
 TweenMax.to("#sun", 10, {opacity:0, y: 600,  repeat:-1, yoyo:true});
 TweenMax.from("#moon", 10, {opacity:0, y: 600,  repeat:-1, yoyo:true});
 
-TweenMax.to(".body", 10, {backgroundColor:"#0C0C38", repeat:-1, yoyo:true});
+// background color
+TweenMax.to(".body", 10, {backgroundColor:"#06061C", repeat:-1, yoyo:true});
 
 
+//date
+
+var d = new Date();
+document.getElementById("date").innerHTML = d.toDateString();
+
+var date = document.getElementById('date');
+
+var tl2 = new TimelineMax();
+tl2.to(date, 1, {top: 200});
